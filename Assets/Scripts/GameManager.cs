8925b5f9f232ea2f5e9a6dbcfe13ee64df587dff
Tonda <ant.Hojny@gmail.com>
2018-01-27 01:59:55 +0100

    Zoom Player

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    static GameManager Instance;
+
+    public Transform actualRobot;
+	// Use this for initialization
+	void Start () {
+        Instance = this;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
