07ef7923ab016d0b70e61f7d1c0d23a100424a53
Tonda <ant.Hojny@gmail.com>
2018-01-27 13:52:55 +0100

    AI shiting

---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour {
+    public GameObject Explosion;
+    public AIBase owner;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter( Collision coll)
+    {
+        Instantiate(Explosion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        VRPlayer player = coll.gameObject.GetComponent<VRPlayer>();
+        if (player)
+        {
+            player.Die();
+            if(owner)
+            {
+                owner.KillTarger();
+            }
+            Instantiate(Explosion, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
