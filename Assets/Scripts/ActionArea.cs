ccf50af04c25bd84adefc16d76e2e86c7cd1e81c
VRena-Meryl/Legacycz <snake2112@seznam.cz>
2018-01-28 11:41:12 +0100

    Minor bugfixes.

---
+++ Assets/Scripts/ActionArea.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ActionArea : MonoBehaviour
+{
+    public UnityEvent OnBlockStepped;
+    public AudioSource thisAudioSource;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.gameObject.name);
+
+        if (other.tag == "Player")
+        {
+            OnBlockStepped.Invoke();
+        }
+    }
+}
